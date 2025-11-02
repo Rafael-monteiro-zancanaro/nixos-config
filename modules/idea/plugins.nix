017bed916b594cc904099f2a1c8b1eb3587ee59f
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2025-11-02 00:05:47 -0300

    [Feat]: Isolando idea-community

--- modules/idea/plugins.nix
+++ modules/idea/plugins.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  programs.idea-community.addPlugins = with pkgs.jetbrains.plugins; [
+  packages.idea-community.addPlugins = with pkgs.jetbrains.plugins; [
     "ideavim"
   ];
 }
