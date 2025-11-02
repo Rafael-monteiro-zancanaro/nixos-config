d1af03b21e2ca83324c4bf7f1a14be87f0caeb5e
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2025-11-01 22:46:29 -0300

    [Feat]: Movendo arquivos vscode para configuração isolada

--- modules/vscode/profiles.nix
+++ modules/vscode/profiles.nix
@@ -4,21 +4,17 @@
     default = {
       extensions = with pkgs.vscode-extensions; [
         vscodevim.vim
- #       jnoortheen.nix-ide
+        jnoortheen.nix-ide
       ];
-      userSettings = {
- #       "vim.normalModeKeyBindings" = ''
- #       [ { "before": ["<Space>"], "after": [":"] } ]
- #       '';
- #       "vim.insertModeKeyBindings" = ''
- #       [ { "before": ["j", "k"], "after": ["<Esc>"] } ]
- #       '';
-        "workbench.sideBar.location" = "right";
- #       "vim.useSystemClipboard" = "true";
- #       "vim.handleKeys" = ''
- #       { "<C-d>": true, "<C-s>": false, "<C-z>": false, "<C-f>": false, "<C-p>": false }
- #       '';
-      };
+      userSettings = builtins.fromJSON ''
+	{
+	    "workbench.sideBar.location": "right",
+	    "vim.useSystemClipboard": true,
+	    "vim.normalModeKeyBindings": [ { "before": ["<Space>"], "after": [":"] } ],
+	    "vim.insertModeKeyBindings": [ { "before": ["j", "k"], "after": ["<Esc>"] } ],
+	    "vim.handleKeys": { "<C-d>": true, "<C-s>": false, "<C-z>": false, "<C-f>": false, "<C-p>": false }
+	}	
+      ''
     };
   };
 }
