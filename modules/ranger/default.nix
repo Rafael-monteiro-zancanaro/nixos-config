97331c189770356c62732b93a534605c31958f9c
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2025-11-14 22:27:13 -0300

    [Feat]: Adding i3 (i wanna be a nerd too >:( )

--- modules/ranger/default.nix
+++ modules/ranger/default.nix
@@ -4,7 +4,24 @@
   programs.ranger = {
     enable = true;
     
-    # Configurações básicas
+    # Configurações que vão para o rc.conf
+    extraConfig = ''
+      # Custom keybindings
+      map gh cd ~
+      map gr cd /
+      map gD cd ~/Downloads
+      map gM cd ~/Documents
+      map gC cd ~/.config
+      map gP cd ~/projects
+      
+      # Toggle hidden files
+      map zh set show_hidden!
+      
+      # Better file sorting
+      set sort natural
+    '';
+    
+    # Settings que vão para o commands.py
     settings = {
       show_hidden = true;
       preview_images = true;
@@ -13,29 +30,28 @@
     };
   };
   
-  # Keybindings customizados via arquivo rc.conf
-  home.file.".config/ranger/rc.conf".text = ''
-    # Custom keybindings
-    map gh cd ~
-    map gr cd /
-    map gD cd ~/Downloads
-    map gM cd ~/Documents
-    map gC cd ~/.config
-    map gP cd ~/projects
-    
-    # Toggle hidden files
-    map zh set show_hidden!
-  '';
-  
-  # Script de preview
+  # Script de preview melhorado (isso vai para scope.sh)
   home.file.".config/ranger/scope.sh".source = pkgs.writeShellScript "scope.sh" ''
     #!/bin/sh
     case "$1" in
-      *.tar*|*.tgz) ${pkgs.atool}/bin/atool -l "$1" ;;
+      *.tar*|*.tgz|*.tbz) ${pkgs.atool}/bin/atool -l "$1" ;;
       *.zip) ${pkgs.unzip}/bin/unzip -l "$1" ;;
+      *.rar) ${pkgs.unrar}/bin/unrar l "$1" ;;
+      *.7z) ${pkgs.p7zip}/bin/7z l "$1" ;;
       *.pdf) ${pkgs.poppler_utils}/bin/pdftotext "$1" - ;;
-      *.jpg|*.jpeg|*.png) ${pkgs.w3m}/bin/w3m -o ext_image_format=png "$1" ;;
-      *) ${pkgs.highlight}/bin/highlight -O ansi "$1" 2>/dev/null || cat "$1" ;;
+      *.mp3|*.flac|*.m4a|*.wav) ${pkgs.mediainfo}/bin/mediainfo "$1" ;;
+      *.jpg|*.jpeg|*.png|*.gif)
+        ${pkgs.w3m}/bin/w3m -o ext_image_format=png "$1" 2>/dev/null ||
+        ${pkgs.imagemagick}/bin/convert - -resize "800x600>" png:-
+        ;;
+      *.mp4|*.avi|*.mkv|*.webm)
+        ${pkgs.ffmpegthumbnailer}/bin/ffmpegthumbnailer -i "$1" -o - -s 800 2>/dev/null
+        ;;
+      *)
+        ${pkgs.bat}/bin/bat --color=always --style=numbers "$1" 2>/dev/null ||
+        ${pkgs.highlight}/bin/highlight -O ansi "$1" 2>/dev/null ||
+        ${pkgs.coreutils}/bin/cat "$1"
+        ;;
     esac
   '';
 }
