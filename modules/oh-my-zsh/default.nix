2f8dd6bf5e175899d6d1aed94ae7415599d6efed
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2025-12-12 14:48:02 -0300

    Adicionando core group

--- modules/oh-my-zsh/default.nix
+++ modules/oh-my-zsh/default.nix
@@ -1,13 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./plugins.nix
+  ];
+
   home.packages = with pkgs; [
     oh-my-zsh
   ];
 
-  programs.oh-my-zsh = {
+  programs.zsh.ohMyZsh = {
     enable = true;
-    plugins = [ ];
     theme = "agnoster";
   };
 }
