9e29b3fe12076b82b2cdd32610499791c0e2636a
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2025-12-12 15:02:14 -0300

    Adicionando zsh config no configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -100,9 +100,10 @@
   programs.zsh = {
     enable = true;
     histSize = 10000;
-    enableSyntaxHighlighting = true;
-    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
     ohMyZsh = {
+        enable = true;
         theme = "agnoster";
         plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" "colorize" ];
     };
