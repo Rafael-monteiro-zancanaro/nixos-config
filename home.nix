ad83740fdbf7383823c47f6f4305ee0596b981e5
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2026-03-06 18:18:32 -0300

    First commit, new environment

---
+++ home.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "zancanaro";
+  home.homeDirectory = "/home/zancanaro";
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    curl
+    htop
+    cmatrix   
+    bat
+    tree
+  ];
+}
