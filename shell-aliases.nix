3d7156cc44b7d1c8d0447cb355a509a5e538393c
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2026-05-02 00:31:47 -0300

    [Feat] Creating file for aliases

--- shell-aliases.nix
+++ shell-aliases.nix
@@ -3,6 +3,7 @@
 {
   programs.bash.enable = true;
   home.shellAliases = {
-    teste = "echo teste testando!!!";
+    flake-templates = "nix flake show github:the-nix-way/dev-templates";
+    flake-template = "nix flake init -t github:the-nix-way/dev-templates#";
   };
 }
