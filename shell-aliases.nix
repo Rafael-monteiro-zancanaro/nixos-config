8410b7478c29f34bbe4bda88104cd930a21d7bb1
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2026-05-02 00:40:54 -0300

    [Feat] Creating file for aliases

--- shell-aliases.nix
+++ shell-aliases.nix
@@ -5,7 +5,12 @@
   home.shellAliases = {
     # Lista todas as listas de templates de flake prontos do repo the-nix-way
     flake-templates = "nix flake show github:the-nix-way/dev-templates";
-    # Baixa um template de flake específico do repo the-nix-way
-    flake-template = "nix flake init -t github:the-nix-way/dev-templates#";
   };
+
+  # Baixa um template de flake específico do repo the-nix-way
+  programs.bash.initExtra = ''
+    flake-template() {
+      nix flake init -t "github:the-nix-way/dev-templates#$1"
+    }
+  '';
 }
