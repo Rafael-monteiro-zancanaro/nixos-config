44cf14bbaf2001018683556a1cef66b852e1e898
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2026-06-27 16:07:35 -0300

    [Feat] generating devenvs by shell alias

--- shell-aliases.nix
+++ shell-aliases.nix
@@ -19,8 +19,8 @@
     }
     envfor() {
       echo "Generating devenv for $*"
-      curl -s -X POST "https://devenv.new/api/generate" \
-        -F "q=$*" \
+      curl -s -X POST -G "https://devenv.new/api/generate" \
+        --data-urlencode "q=$*" \
         | jq -r '.devenv_nix'
     }
   '';
